903760f80e89cad08cde9f4d0b4b5c52ce09009b
Sam Thomas <samthomas@aiml.team>
2024-12-08 23:07:00 +1030

    added chat memeory

--- server/main.py
+++ server/main.py
@@ -32,16 +32,12 @@ class ChatRequest(BaseModel):
 @app.post("/api/chat")
 async def chat(request: ChatRequest):
     try:
+        messages = [msg for msg in request.messages]
         client = Client(
             host='http://localhost:11435',
             headers={'x-some-header': 'some-value'}
                 )
-        response = client.chat(model=request.model, messages=[
-            {
-                'role': 'user',
-                    'content': 'Why is the sky blue?',
-                },
-        ])
+        response = client.chat(model=request.model, messages=messages)
             
 
         return response
